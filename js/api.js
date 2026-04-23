eff416f77ff5127df72652815bb5beb93818f388
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2026-04-23 18:47:46 +0200

    update

--- js/api.js
+++ js/api.js
@@ -5,6 +5,9 @@ const BASE_URL = "http://localhost:8081/cards";
 export async function fetchCards(name, page, size) {
     const url = `${BASE_URL}/search?name=${encodeURIComponent(name)}&page=${page}&size=${size}`;
     const response = await fetch(url);
+    console.log("Buscando:", name);
+    console.log("URL:", url);
     if (!response.ok) throw new Error("Error al obtener cartas");
     return await response.json(); // devuelve CardPageDTO
 }
+
